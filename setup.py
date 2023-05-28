8a8b149310d7eff6aca4ef3d22aaf9bf421d51ad
gs729 <gsfernandes81@gmail.com>
2023-05-28 20:21:09 +0100

    Bump python version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=parse_requirements_file("requirements.txt"),
-    python_requires=">=3.8.0,<3.11",
+    python_requires=">=3.8.0,<3.12",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Framework :: AsyncIO",
